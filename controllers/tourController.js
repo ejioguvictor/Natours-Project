e09e451d72ee9b4c2f95286b8a5bb85f1a3cbf3b
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-03 23:01:44 +0100

    feat: modified the tour model to incude more fields for the tours schema

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,13 +6,13 @@ exports.createTour = async (req, res) => {
     res.status(201).json({
       status: 'success',
       data: {
-        tour: newTour,
-      },
+        tour: newTour
+      }
     });
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'invalid data sent',
+      message: error
     });
   }
 };
@@ -25,13 +25,13 @@ exports.getAllTours = async (req, res) => {
       requestedAt: req.requestTime,
       results: tours.length,
       data: {
-        tours,
-      },
+        tours
+      }
     });
   } catch (error) {
     res.status(404).json({
       status: 'fail',
-      message: error,
+      message: error
     });
   }
 };
@@ -43,13 +43,13 @@ exports.getTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour,
-      },
+        tour
+      }
     });
   } catch (error) {
     res.status(404).json({
       status: 'fail',
-      message: error,
+      message: error
     });
   }
 };
@@ -58,18 +58,18 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true,
+      runValidators: true
     });
     res.status(200).json({
       status: 'success',
       data: {
-        tour,
-      },
+        tour
+      }
     });
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'invalid data sent',
+      message: 'invalid data sent'
     });
   }
 };
@@ -79,12 +79,12 @@ exports.deleteTour = async (req, res) => {
     await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
-      data: null,
+      data: null
     });
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'invalid data sent',
+      message: 'invalid data sent'
     });
   }
 };
