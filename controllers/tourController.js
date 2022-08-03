5d71745721325fe654c828d59c3d12b7ab064228
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-03 21:00:10 +0100

    feat: created the update/patch route handler for to update specified tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,6 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
-
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
@@ -56,13 +55,24 @@ exports.createTour = async (req, res) => {
 
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'invalid data sent',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
