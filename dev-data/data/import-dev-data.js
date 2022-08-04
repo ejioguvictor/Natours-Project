75038c91ed1345165053154f0603cbfedc0706ec
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-04 13:02:31 +0100

    completed data import/delete implrmrntation from json file to mongoose db

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const Tour = require('./../../models/tourModel')
+const Tour = require('./../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -25,27 +25,25 @@ const tours = JSON.parse(
 );
 
 //Importing data into DB
-
 const importData = async () => {
   try {
     await Tour.create(tours);
     console.log('Data successsfully logged to DB');
-    process.exit()
   } catch (error) {
     console.log(error);
   }
+  process.exit();
 };
 
 //Deleting All Data from DB
-
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
     console.log('tours all deleted');
-    process.exit()
   } catch (error) {
     console.log(error);
   }
+  process.exit();
 };
 
 if (process.argv[2] === '--import') {
