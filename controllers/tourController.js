bc3a37e9a526453cacfbda858fd8a5399178c6d9
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-10 12:33:45 +0100

    implemented api filtering aliasing with the aid of a middleware function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
