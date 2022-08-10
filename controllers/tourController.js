8c5270fe94b50cce84bd446845cef43649641f2c
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-10 12:01:13 +0100

    implemented filtering to display only checked fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -38,6 +38,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //Field limiting implementation
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // const tours = await Tour.find();
     const tours = await query;
     res.status(200).json({
