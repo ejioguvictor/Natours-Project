f48b4bbaff5f8fc0813ecb0016b73ef6e0c1acb5
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-12 09:58:34 +0100

    created the route for getting tour statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,8 @@ router
   .route('/top-5-cheap-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
