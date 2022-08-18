516fa3c0945d4cd5a3fe945532963208c50e4f2e
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-18 13:43:33 +0100

    updated error handling files

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -5,7 +12,7 @@ const sendErrorDev = (err, res) => {
     stack: err.stack,
     error: err
   });
-}
+};
 
 const sendErrorProd = (err, res) => {
   //Operational, trusted error: send message to client
@@ -31,6 +38,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
